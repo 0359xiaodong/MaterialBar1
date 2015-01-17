20a53ad09920d0dea0266252bd79ffa9e1fd916b
yanglw <weigeku8@gmail.com>
2015-01-17 16:06:42 +0800

    初始提交

---
+++ app/src/main/java/com/github/yanglw/materialbar1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.yanglw.materialbar1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_main);
+    }
+}
